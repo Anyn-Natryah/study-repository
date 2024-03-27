6d826f13fef5691fec7c9e438118b78ce4b963b4
Anyn-Natryah <165163475+Anyn-Natryah@users.noreply.github.com>
2024-03-27 09:35:54 -0300

    Upload tiled project files

---
+++ tiled projects/grass_teal_overlay_256x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="grass_teal_overlay_256x" tilewidth="85" tileheight="85" tilecount="9" columns="3">
+ <image source="grass_256x_v2/grass 256x v2/grass_teal_overlay_256x.png" trans="000000" width="256" height="256"/>
+</tileset>
